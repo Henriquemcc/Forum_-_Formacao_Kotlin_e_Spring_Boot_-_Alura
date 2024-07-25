00eef919aca99d40c691237637b14b11bc85fb0c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-07-25 17:09:48 -0300

    Adicionando classes do modelo. (Parte 2 da aula 2 do curso 'API REST com Kotlin e Spring Boot: Camada Web')

---
+++ src/main/kotlin/io/github/henriquemcc/forum/model/Curso.kt
@@ -0,0 +1,7 @@
+package io.github.henriquemcc.forum.model
+
+data class Curso(
+    val id: Long? = null,
+    val nome: String,
+    val categoria: String
+)
---
+++ src/main/kotlin/io/github/henriquemcc/forum/model/Usuario.kt
@@ -0,0 +1,7 @@
+package io.github.henriquemcc.forum.model
+
+data class Usuario(
+    val id: Long? = null,
+    val nome: String,
+    val email: String
+)
