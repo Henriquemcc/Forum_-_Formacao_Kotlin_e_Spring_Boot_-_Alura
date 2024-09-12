cdf3f9a915ca0621699ad467450cc8a74cc149ef
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-09-12 19:28:05 -0300

    Implementando tratamento de erros de validação. (Parte 6 da aula 5 do curso: 'API REST com Kotlin e Spring Boot: Camada Web')

--- src/main/kotlin/io/github/henriquemcc/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/exception/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package io.github.henriquemcc.forum.exception
 import io.github.henriquemcc.forum.dto.ErrorView
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -33,6 +34,20 @@ class ExceptionHandler {
             status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
             error = HttpStatus.INTERNAL_SERVER_ERROR.name,
             message = exception.message ?: "",
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = exception.message,
             path = request.servletPath
         )
     }
