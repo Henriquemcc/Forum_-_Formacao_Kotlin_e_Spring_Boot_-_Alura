d3c7ced2020c9a0e36412b93f1445a93f72f6cfd
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-09-15 14:05:13 -0300

    Corrigindo erro no qual o Spring Boot não inicializava

--- src/main/kotlin/io/github/henriquemcc/forum/model/Curso.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/model/Curso.kt
@@ -1,9 +1,11 @@
 package io.github.henriquemcc.forum.model
 
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
+@Entity
 data class Curso(
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
