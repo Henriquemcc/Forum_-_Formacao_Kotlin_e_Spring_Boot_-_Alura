2c7a0c96466ac0d0a20c9086732260a6f8d02307
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-09-15 13:30:46 -0300

    Mapeando as entidades JPA. (Parte 6 da aula 1 do curso: 'API REST com Kotlin e Spring Boot: Camada de persistência')

--- src/main/kotlin/io/github/henriquemcc/forum/model/Curso.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/model/Curso.kt
@@ -1,7 +1,14 @@
 package io.github.henriquemcc.forum.model
 
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
 data class Curso(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
     val nome: String,
     val categoria: String
 )
