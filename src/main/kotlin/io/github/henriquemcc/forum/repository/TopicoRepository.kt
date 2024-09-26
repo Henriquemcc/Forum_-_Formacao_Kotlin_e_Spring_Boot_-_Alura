c82bf25ec4ef62c1ccf9ecee657493dae0eef1f3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-09-26 10:52:28 -0300

    Removendo código morto

--- src/main/kotlin/io/github/henriquemcc/forum/repository/TopicoRepository.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/repository/TopicoRepository.kt
@@ -15,21 +15,6 @@ interface TopicoRepository: JpaRepository<Topico, Long> {
     @Query("SELECT new io.github.henriquemcc.forum.dto.TopicoPorCategoriaDto(curso.categoria, count(t)) FROM Topico t JOIN t.curso curso GROUP BY curso.categoria")
     fun relatorio(): List<TopicoPorCategoriaDto>
 
-//    @Query("SELECT new io.github.henriquemcc.forum.dto.TopicoNaoRespondidoDto(t.titulo, curso.nome, autor.nome, t.dataCriacao)" +
-//            "FROM Topico t" +
-//            "JOIN t.curso curso" +
-//            "JOIN t.autor autor" +
-//            "WHERE t.respostas IS EMPTY" +
-//            "GROUP BY t.dataCriacao"
-//            )
-//    fun relatorioTopicosNaoRespondidos(): List<TopicoNaoRespondidoDto>
-
-//    @Query("SELECT t from Topico t where t.respostas is empty")
-//    fun relatorioTopicosNaoRespondidos(): List<Topico>
-
-//    @Query("SELECT new io.github.henriquemcc.forum.dto.TopicoView(t.id, t.titulo, t.mensagem, t.status, t.dataCriacao) from Topico t where t.respostas is empty")
-//    fun relatorioTopicosNaoRespondidos(): List<TopicoView>
-
     @Query("SELECT new io.github.henriquemcc.forum.dto.TopicoNaoRespondidoDto(t.titulo, t.curso.nome, t.autor.nome, t.dataCriacao) from Topico t where t.respostas is empty ORDER BY t.dataCriacao")
     fun relatorioTopicosNaoRespondidos(): List<TopicoNaoRespondidoDto>
 }
