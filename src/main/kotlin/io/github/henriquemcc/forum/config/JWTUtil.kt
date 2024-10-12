dbacd0934b7ccdfa99c055ba847949ad47b084bc
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-12 16:26:00 -0300

    Criando a classe JWTUtil e adicionando um valor para 'jwt.secret'. (Parte 4 da aula 3 do curso 'Kotlin e Spring: segurança e infraestrutura')

---
+++ src/main/kotlin/io/github/henriquemcc/forum/config/JWTUtil.kt
@@ -0,0 +1,26 @@
+package io.github.henriquemcc.forum.config
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.Jwts.SIG
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys.hmacShaKeyFor
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JWTUtil {
+
+    private val expiration: Long = 60000
+
+    @Value("\${jwt.secret}")
+    private lateinit var secret: String
+
+    fun generateToken(username: String): String? {
+        return Jwts.builder().
+        subject(username).
+        expiration(Date(System.currentTimeMillis() + expiration)).
+        signWith(hmacShaKeyFor(secret.toByteArray()), SIG.HS512).
+        compact()
+    }
+}
