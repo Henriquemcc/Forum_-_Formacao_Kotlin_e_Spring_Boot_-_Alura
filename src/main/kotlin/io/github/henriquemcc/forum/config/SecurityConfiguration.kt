5e8a021829b2e79b47da3ba7d167faad2c285faf
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-01-02 21:50:29 -0300

    Configurando o Json Web Token. (Parte 5 da aula 3 do curso 'Kotlin e Spring: segurança e infraestrutura')

--- src/main/kotlin/io/github/henriquemcc/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/config/SecurityConfiguration.kt
@@ -1,9 +1,13 @@
 package io.github.henriquemcc.forum.config
 
+import io.github.henriquemcc.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -11,12 +15,16 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
 
 @Configuration
 @EnableWebSecurity
 // https://cursos.alura.com.br/forum/topico-websecurityconfigureradapter-deprecated-nova-implementacao-226360
 // https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
 class SecurityConfiguration(
+    // https://cursos.alura.com.br/forum/topico-para-resolver-o-authenticationmanager-441881
+    private val authenticationConfiguration: AuthenticationConfiguration,
     private val userDetailsService: UserDetailsService,
     private val jwtUtil: JWTUtil
 ) {
@@ -32,17 +40,17 @@ class SecurityConfiguration(
     fun filterChain(http: HttpSecurity): SecurityFilterChain? {
 
         // https://spring.io/blog/2019/11/21/spring-security-lambda-dsl
-        return http.
-        csrf {it.disable ()}.
-        authorizeHttpRequests {
+        http.csrf {it.disable ()}
+        http.authorizeHttpRequests {
             //it.requestMatchers(HttpMethod.GET, "/topicos").hasAuthority("LEITURA_ESCRITA").anyRequest().authenticated()
             it.requestMatchers(HttpMethod.POST, "/login").permitAll()
-        }.
-        addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass).
-        sessionManagement {
+        }
+        http.addFilterBefore(JWTLoginFilter(authManager = authenticationConfiguration.authenticationManager, jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http.sessionManagement {
             it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        }.
-        httpBasic(Customizer.withDefaults()).build()
-    }
+        }
+        http.httpBasic(Customizer.withDefaults())
+        return http.build()
 
+    }
 }
