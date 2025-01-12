62261b15df1a46ea04778846e544c51de5a60c54
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-01-12 17:05:25 -0300

    Implementando RespostaDtoService

--- src/main/kotlin/io/github/henriquemcc/forum/controller/RespostaController.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/controller/RespostaController.kt
@@ -3,6 +3,7 @@ package io.github.henriquemcc.forum.controller
 import io.github.henriquemcc.forum.dto.AtualizarRespostaForm
 import io.github.henriquemcc.forum.dto.NovaRespostaForm
 import io.github.henriquemcc.forum.dto.RespostaView
+import io.github.henriquemcc.forum.service.RespostaDtoService
 import io.github.henriquemcc.forum.service.RespostaService
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
@@ -13,16 +14,16 @@ import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/topicos/{idTopico}/respostas")
-class RespostaController(private val service: RespostaService) {
+class RespostaController(private val respostaDtoService: RespostaDtoService) {
 
     @GetMapping
     fun listarPorIdTopico(@PathVariable idTopico: Long): List<RespostaView> {
-        return service.listarPorIdTopicoListRespostaView(idTopico)
+        return respostaDtoService.listarPorIdTopico(idTopico)
     }
 
     @GetMapping("/{idResposta}")
     fun buscarPorIdResposta(@PathVariable idTopico: Long, @PathVariable idResposta: Long): RespostaView {
-        return service.buscarPorIdRespostaView(idTopico, idResposta)
+        return respostaDtoService.buscarPorId(idTopico, idResposta)
     }
 
     @PostMapping
@@ -32,7 +33,7 @@ class RespostaController(private val service: RespostaService) {
         @PathVariable idTopico: Long,
         uriBuilder: UriComponentsBuilder
     ): ResponseEntity<RespostaView> {
-        val respostaView = service.cadastrar(form, idTopico)
+        val respostaView = respostaDtoService.cadastrar(form, idTopico)
         val uri = uriBuilder.path("/topicos/${idTopico}/respostas").build().toUri()
         return ResponseEntity.created(uri).body(respostaView)
     }
@@ -43,7 +44,7 @@ class RespostaController(private val service: RespostaService) {
         @RequestBody @Valid form: AtualizarRespostaForm,
         @PathVariable idResposta: Long
     ): ResponseEntity<RespostaView> {
-        val respostaView = service.atualizar(form, idResposta)
+        val respostaView = respostaDtoService.atualizar(form, idResposta)
         return ResponseEntity.ok(respostaView)
     }
 
@@ -51,6 +52,6 @@ class RespostaController(private val service: RespostaService) {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
     fun deletar(@PathVariable idResposta: Long) {
-        service.deletar(idResposta)
+        respostaDtoService.deletar(idResposta)
     }
 }
--- src/main/kotlin/io/github/henriquemcc/forum/mapper/NovaRespostaFormMapper.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/mapper/NovaRespostaFormMapper.kt
@@ -2,14 +2,12 @@ package io.github.henriquemcc.forum.mapper
 
 import io.github.henriquemcc.forum.dto.NovaRespostaForm
 import io.github.henriquemcc.forum.model.Resposta
-import io.github.henriquemcc.forum.service.TopicoService
 import io.github.henriquemcc.forum.service.UsuarioService
 import org.springframework.stereotype.Component
 
 @Component
-class RespostaFormMapper(
+class NovaRespostaFormMapper(
     val usuarioService: UsuarioService,
-    val topicoService: TopicoService,
 ) : Mapper<NovaRespostaForm, Resposta> {
 
     override fun map(t: NovaRespostaForm): Resposta {
