d43645e86b8fa3ddd277491bcb7b5a34437b0170
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-01-21 20:32:35 -0300

    Adicionando criação do container do MySQL no Docker e a integração entre a parte de testes da aplicação e o container. (Parte 4 da aula 3 do curso 'Kotlin e Spring: testes automatizados e documentação de API')

--- src/test/kotlin/io/github/henriquemcc/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/io/github/henriquemcc/forum/integration/TopicoRepositoryTest.kt
@@ -2,9 +2,43 @@ package io.github.henriquemcc.forum.integration
 
 import io.github.henriquemcc.forum.repository.TopicoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TopicoRepositoryTest {
 
     @Autowired
     private lateinit var topicoRepository: TopicoRepository
+
+    companion object {
+
+        /**
+         * Cria o container do MySQL no Docker.
+         */
+        @Container
+        private val mysqlContainer = MySQLContainer<Nothing>("mysql:9.1.0").apply{
+            withDatabaseName("testedb")
+            withUsername("myuser")
+            withPassword("secret")
+        }
+
+        /**
+         * Define as propriedades equivalentes ao 'application.properties'.
+         */
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
+            registry.add("spring.datasource.password", mysqlContainer::getPassword)
+            registry.add("spring.datasource.username", mysqlContainer::getUsername)
+        }
+    }
 }
