4a47cf415485267d822a1d2a5cb641c830cc5a53
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-01-22 19:14:04 -0300

    Corrigindo erro no qual o campo topicoRepository não era inicializado.

--- src/test/kotlin/io/github/henriquemcc/forum/integration/TopicoRepositoryTest.kt
+++ src/test/kotlin/io/github/henriquemcc/forum/integration/TopicoRepositoryTest.kt
@@ -8,12 +8,14 @@ import org.junit.ClassRule
 import org.junit.Test
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.junit4.SpringRunner
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -21,10 +23,8 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @DataJpaTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@RunWith(SpringRunner::class)
 class TopicoRepositoryTest {
-
-    @Autowired
-    private lateinit var topicoRepository: TopicoRepository
 
     private val topico = TopicoTest.build()
 
@@ -54,6 +54,9 @@ class TopicoRepositoryTest {
             registry.add("spring.datasource.driverClassName", mysqlContainer::getDriverClassName)
         }
     }
+
+    @Autowired
+    private lateinit var topicoRepository: TopicoRepository
 
     @Test
     fun `deve gerar um relatorio`() {
