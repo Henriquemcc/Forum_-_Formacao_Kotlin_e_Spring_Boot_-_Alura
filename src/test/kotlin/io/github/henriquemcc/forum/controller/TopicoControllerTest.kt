e70b6c121717a0529a638fbd346a118e22b5a8b2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-01-23 17:16:26 -0300

    Adicionando inicialização do mockMvc. (Parte 2 da aula 4 do curso 'Kotlin e Spring: testes automatizados e documentação de API')

--- src/test/kotlin/io/github/henriquemcc/forum/controller/TopicoControllerTest.kt
+++ src/test/kotlin/io/github/henriquemcc/forum/controller/TopicoControllerTest.kt
@@ -1,12 +1,16 @@
 package io.github.henriquemcc.forum.controller
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 
 
 // https://github.com/spring-projects/spring-security-samples/blob/main/servlet/spring-boot/java/hello-security-explicit/src/test/java/example/HelloSecurityExplicitApplicationTests.java
@@ -19,6 +23,11 @@ class TopicoControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity()).build()
+    }
 
     companion object {
         private const val RECURSO = "/topicos/"
