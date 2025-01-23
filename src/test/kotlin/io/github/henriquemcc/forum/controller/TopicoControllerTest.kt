c0399850d2ef4f3fcc513b96cd6f00bf3f440ff6
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-01-23 16:33:22 -0300

    Adicionando caso de teste no qual testa se acontece erro do tipo 400 ao chamar topicos sem o token. (Está dando erro) (Parte 3 da aula 4 do curso 'Kotlin e Spring: testes automatizados e documentação de API')

--- src/test/kotlin/io/github/henriquemcc/forum/controller/TopicoControllerTest.kt
+++ src/test/kotlin/io/github/henriquemcc/forum/controller/TopicoControllerTest.kt
@@ -1,10 +1,13 @@
 package io.github.henriquemcc.forum.controller
 
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.web.context.WebApplicationContext
+
 
 // https://github.com/spring-projects/spring-security-samples/blob/main/servlet/spring-boot/java/hello-security-explicit/src/test/java/example/HelloSecurityExplicitApplicationTests.java
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,4 +19,13 @@ class TopicoControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    companion object {
+        private const val RECURSO = "/topicos/"
+    }
+
+    @Test
+    fun `deve retornar codigo 400 quando chamar topicos sem token`() {
+        mockMvc.get(RECURSO).andExpect { status { is4xxClientError() } }
+    }
 }
