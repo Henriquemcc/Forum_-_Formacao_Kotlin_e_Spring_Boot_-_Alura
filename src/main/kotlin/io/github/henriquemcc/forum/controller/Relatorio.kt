b7f0f8c7956c291fe3dedd6ec4d46e427dba640f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-02-02 17:59:07 -0300

    Configurando o controlador para utilizar o thymeleaf. (Não está funcionando corretamente) (Parte 3 da aula 3 do curso 'Kotlin e Spring: recursos avançados')

--- src/main/kotlin/io/github/henriquemcc/forum/controller/Relatorio.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/controller/Relatorio.kt
@@ -1,20 +1,21 @@
 package io.github.henriquemcc.forum.controller
 
-import io.github.henriquemcc.forum.dto.TopicoPorCategoriaDto
 import io.github.henriquemcc.forum.service.TopicoService
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@RequestMapping("/relatorios")
+@Controller
+@RequestMapping("relatorios")
 @SecurityRequirement(name = "bearerAuth")
 class Relatorio(
     private val topicoService: TopicoService
 ) {
     @GetMapping
-    fun relatorio(): List<TopicoPorCategoriaDto> {
-        return topicoService.relatorio()
+    fun relatorio(model: Model): String {
+        model.addAttribute("topicosPorCategorias", topicoService.relatorio())
+        return "relatorio"
     }
 }
