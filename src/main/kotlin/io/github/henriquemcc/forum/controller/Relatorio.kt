be595146b19bf5229cf5f9751decdeb1333d3987
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-02-02 17:04:52 -0300

    Adicionando requisito de segurança

--- src/main/kotlin/io/github/henriquemcc/forum/controller/Relatorio.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/controller/Relatorio.kt
@@ -2,12 +2,14 @@ package io.github.henriquemcc.forum.controller
 
 import io.github.henriquemcc.forum.dto.TopicoPorCategoriaDto
 import io.github.henriquemcc.forum.service.TopicoService
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/relatorios")
+@SecurityRequirement(name = "bearerAuth")
 class Relatorio(
     private val topicoService: TopicoService
 ) {
