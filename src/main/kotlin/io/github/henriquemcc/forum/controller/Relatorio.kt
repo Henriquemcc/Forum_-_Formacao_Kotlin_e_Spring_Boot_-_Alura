123e2197fb8cbdbd3c830d9700f497f819a3cc3e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-02-02 16:59:12 -0300

    Adicionando rota '/relatorios'. (Parte 2 da aula 3 do curso 'Kotlin e Spring: recursos avançados')

--- src/main/kotlin/io/github/henriquemcc/forum/controller/Relatorio.kt
+++ src/main/kotlin/io/github/henriquemcc/forum/controller/Relatorio.kt
@@ -3,9 +3,11 @@ package io.github.henriquemcc.forum.controller
 import io.github.henriquemcc.forum.dto.TopicoPorCategoriaDto
 import io.github.henriquemcc.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/relatorios")
 class Relatorio(
     private val topicoService: TopicoService
 ) {
